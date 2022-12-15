1800dca55ab8bb778f00395270f4a8747384434f
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-12-15 22:40:32 +0700

    feat: add crash-test

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,13 @@ app.use(bodyParser.json());
 
 app.use(requestLogger);
 
+// Краш-тест сервера
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // подключение всех роутов
 app.use(router);
 
