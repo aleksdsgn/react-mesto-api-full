01a16b65141e0f739b953204bf0fcc726e532f26
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-12-17 12:44:11 +0700

    fix: auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,6 +8,7 @@ export const auth = (req, res, next) => {
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization) {
     next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
   // извлечём токен
   const token = authorization.replace(/^Bearer*\s*/i, '');
@@ -26,27 +27,3 @@ export const auth = (req, res, next) => {
   // пропускаем запрос дальше
   next();
 };
-
-// Способ наставника Сергея
-// export const auth = (req, res, next) => {
-//   // достаём авторизационный заголовок
-//   const { authorization = '' } = req.headers;
-
-//   // убеждаемся, что он есть или начинается с Bearer
-//   if (!authorization) {
-//     next(new UnauthorizedError('Необходима авторизация'));
-//   } else {
-//     // извлечём токен
-//     const token = authorization.replace(/^Bearer*\s*/i, '');
-//     const { JWT_SALT } = req.app.get('config');
-//     try {
-//       // пытаемся верифицировать токен
-//       const decoder = jwt.verify(token, JWT_SALT);
-//       req.user = { _id: decoder._id };
-//       next();
-//     } catch (err) {
-//       // отправим ошибку, если не получилось
-//       next(new UnauthorizedError('Токен не верифицирован 12'));
-//     }
-//   }
-// };
